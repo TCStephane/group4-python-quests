f1fef6fcfb81b4f615b7bd59d52247828df0bf5c
Joel N <n.joel@alustudent.com>
2026-02-25 15:12:35 +0200

    Hi

---
+++ quests/quest_14_the_logical_gatekeeper.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+goldvalue = int(input("Enter the value of gold you have: "))
+if  age >= 18 and goldvalue >= 20:
+    print("You can enter the castle")
+else:
+    print("You cannot enter the castle")
